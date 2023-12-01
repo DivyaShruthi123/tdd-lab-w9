2db43c4edb058d672dcbab68499802c29434d1bd
DivyaShruthi123 <shruthi387@gmail.com>
2023-11-30 21:00:50 -0500

    commit

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,6 +1,8 @@
 # src/tests/conftest.py
 import pytest
-from src import create_app, db # updated
+from src import create_app,db # updated
+from src.api.models import User
+
 @pytest.fixture(scope='module')
 def test_app():
  app = create_app() # new
@@ -13,3 +15,12 @@ def test_database():
  yield db # testing happens here
  db.session.remove()
  db.drop_all()
+
+@pytest.fixture(scope='function')
+def add_user():
+ def _add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+ return _add_user
