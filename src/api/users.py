dad19fe4a6c54dc673ea3045cf76ea6af34ebbf5
DivyaShruthi123 <shruthi387@gmail.com>
2023-12-01 17:38:02 -0500

    commit

--- src/api/users.py
+++ src/api/users.py
@@ -23,6 +23,31 @@ class Users(Resource):
     if not user:
         api.abort(404, f"User {user_id} does not exist")
     return user, 200
+ @api.expect(user, validate=True)
+ @api.marshal_with(user)
+ def put(self, user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        api.abort(404, f"User {user_id} does not exist")
+
+    # Update user fields based on the request data
+    update_data = request.get_json()
+    user.username = update_data.get('username', user.username)
+    user.email = update_data.get('email', user.email)
+
+    db.session.commit()
+    return user, 200
+
+ def delete(self, user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        api.abort(404, f"User {user_id} does not exist")
+
+    db.session.delete(user)
+    db.session.commit()
+
+    response_object = {'message': f'User {user_id} has been deleted'}
+    return response_object, 200
 
 class UsersList(Resource):
  @api.marshal_with(user, as_list=True)
